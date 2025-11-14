e74153f4f8ffe61ff5bc6df3f34a801f7b229924
Hernandes de Castro Guereta <132833897+HernandesGuereta@users.noreply.github.com>
2025-11-14 01:14:45 +0000

    Codigo HTML/CSS parcialmente pronto

--- telas-uniplanning/src/app/materia/materia-module.ts
+++ telas-uniplanning/src/app/materia/materia-module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { MatSelectModule } from '@angular/material/select';
 import { MateriaRoutingModule } from './materia-routing-module';
 import { CadastrarMateria } from './cadastrar-materia/cadastrar-materia';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +18,14 @@ import { CadastrarMateria } from './cadastrar-materia/cadastrar-materia';
   imports: [
     CommonModule,
     MateriaRoutingModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class MateriaModule { }
--- telas-uniplanning/src/app/professor/professor-module.ts
+++ telas-uniplanning/src/app/professor/professor-module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { ProfessorRoutingModule } from './professor-routing-module';
 import { CadastrarProfessor } from './cadastrar-professor/cadastrar-professor';
@@ -11,7 +18,15 @@ import { CadastrarProfessor } from './cadastrar-professor/cadastrar-professor';
   ],
   imports: [
     CommonModule,
-    ProfessorRoutingModule
+    ProfessorRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class ProfessorModule { }
